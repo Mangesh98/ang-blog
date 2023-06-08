ab7987f10f21ae4eb01cb0fd845446a5b616c44b
Mangesh98 <mangeshraje55555@gmail.com>
2023-06-08 08:04:45 +0530

    Created New app for backend dashbord   designed all the component   Added firebase

--- back-end-dashboard/src/app/app.module.ts
+++ back-end-dashboard/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ back-end-dashboard/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ang-blog-a3cc2',
+    appId: '1:915378859731:web:35c6b65cbb940a18382e26',
+    storageBucket: 'ang-blog-a3cc2.appspot.com',
+    apiKey: 'AIzaSyCyTMH3wXDMLbOte7665YTRke8d-bBZpeQ',
+    authDomain: 'ang-blog-a3cc2.firebaseapp.com',
+    messagingSenderId: '915378859731',
+  },};
